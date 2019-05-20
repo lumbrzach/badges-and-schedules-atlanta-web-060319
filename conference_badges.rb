27f199b527eab5615fd3c401eee12debe6c28881
Zach Parker <parkerz410@gmail.com>
2019-05-20 18:16:39 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -9,6 +9,5 @@ def batch_badge_creator (luminaries)
 end
 
 def assign_rooms(attendees)
-  room_assignments = (1..7).to_a
-  attendees.collect {|i| "Hello, #{attendees[i]}! You'll be assigned to room #{room_assignments[i]}!"}
+  attendees.map {|i| "Hello, #{attendees[i]}! You'll be assigned to room #{room_assignments[i]}!"}
 end
