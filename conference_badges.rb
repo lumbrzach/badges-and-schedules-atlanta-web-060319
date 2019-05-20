d94292372e02e141546863e95c2aaa50e49e8994
Zach Parker <parkerz410@gmail.com>
2019-05-20 18:03:43 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -11,6 +11,6 @@ end
 def assign_rooms(attendees)
   room_assignments = (1..7).to_a
   attendees.map do |i|
-    "Hello, #{attendees[0]}! You'll be assigned to room #{room_assignments[0]}!"
+    "Hello, #{attendees[x]}! You'll be assigned to room #{room_assignments[x]}!"
   end
 end
