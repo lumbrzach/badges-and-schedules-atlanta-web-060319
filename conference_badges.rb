676024f22ddd6be4045db21025d66caa7c05a980
Zach Parker <parkerz410@gmail.com>
2019-05-20 17:43:40 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,10 @@
-# Write your code here.
+def badge_maker (name)
+  puts "Hello, my name is #{name}."
+end
+
+def batch_badge_creator (luminaries)
+  luminaries.each |i| do 
+    badge_maker(i)
+  end
+end
+
