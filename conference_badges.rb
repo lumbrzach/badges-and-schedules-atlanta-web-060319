93aa68f26c753e908b589d00b0917371f9ee7dc4
Zach Parker <parkerz410@gmail.com>
2019-05-20 18:43:20 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -13,7 +13,7 @@ def assign_rooms(attendees)
 end
 
 def printer (attendees)
-  batch_badge_creator (attendees)
+  puts batch_badge_creator (attendees)
   assign_rooms(attendees).each do |i|
     puts i
   end
